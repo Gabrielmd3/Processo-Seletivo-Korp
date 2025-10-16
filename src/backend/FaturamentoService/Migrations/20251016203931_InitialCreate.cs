c2be3bb063734c4dd7029f81ff5d07998d4b85df
Gabriel Delfes <106628024+Gabrielmd3@users.noreply.github.com>
2025-10-16 20:46:24 -0300

    chore(faturamento): Criar migração para as tabelas NotaFiscal e NotaFiscalItens

---
+++ src/backend/FaturamentoService/Migrations/20251016203931_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FaturamentoService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotasFiscais",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Numero = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotasFiscais", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotaFiscalItens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProdutoId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantidade = table.Column<int>(type: "integer", nullable: false),
+                    PrecoUnitario = table.Column<decimal>(type: "numeric", nullable: false),
+                    NotaFiscalId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotaFiscalItens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NotaFiscalItens_NotasFiscais_NotaFiscalId",
+                        column: x => x.NotaFiscalId,
+                        principalTable: "NotasFiscais",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotaFiscalItens_NotaFiscalId",
+                table: "NotaFiscalItens",
+                column: "NotaFiscalId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotaFiscalItens");
+
+            migrationBuilder.DropTable(
+                name: "NotasFiscais");
+        }
+    }
+}
---
+++ src/backend/FaturamentoService/Migrations/20251016214326_ModificacaoTabelasDeFaturamento.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FaturamentoService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModificacaoTabelasDeFaturamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NotaFiscalItens_NotasFiscais_NotaFiscalId",
+                table: "NotaFiscalItens");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataEmissao",
+                table: "NotasFiscais",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "NotaFiscalId",
+                table: "NotaFiscalItens",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NotaFiscalItens_NotasFiscais_NotaFiscalId",
+                table: "NotaFiscalItens",
+                column: "NotaFiscalId",
+                principalTable: "NotasFiscais",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NotaFiscalItens_NotasFiscais_NotaFiscalId",
+                table: "NotaFiscalItens");
+
+            migrationBuilder.DropColumn(
+                name: "DataEmissao",
+                table: "NotasFiscais");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "NotaFiscalId",
+                table: "NotaFiscalItens",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NotaFiscalItens_NotasFiscais_NotaFiscalId",
+                table: "NotaFiscalItens",
+                column: "NotaFiscalId",
+                principalTable: "NotasFiscais",
+                principalColumn: "Id");
+        }
+    }
+}
